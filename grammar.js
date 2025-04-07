48bf7f73eeb65ecfc9dfcfa0c4f17e5503713b28
Firas al-Khalil <firasalkhalil@gmail.com>
2025-04-07 15:17:12 +0200

    feat: Single Quote for TSQL (N prefix)

--- grammar.js
+++ grammar.js
@@ -3552,7 +3552,7 @@ module.exports = grammar({
     _double_quote_string: _ => /"[^"]*"/,
     // The norm specify that between two consecutive string must be a return,
     // but this is good enough.
-    _single_quote_string: _ => seq(/([uU]&)?'([^']|'')*'/, repeat(/'([^']|'')*'/)),
+    _single_quote_string: _ => seq(/([uU]&|[nN])?'([^']|'')*'/, repeat(/'([^']|'')*'/)),
 
     _postgres_escape_string: _ => /(e|E)'([^']|\\')*'/,
 
