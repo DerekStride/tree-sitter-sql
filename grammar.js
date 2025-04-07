7f0b76c935333d287abc35599fa6f7d28175c3fb
Firas al-Khalil <firasalkhalil@gmail.com>
2025-04-07 14:39:44 +0200

    feat: TSQL OBJECT_ID

--- grammar.js
+++ grammar.js
@@ -328,6 +328,7 @@ module.exports = grammar({
     keyword_statement: _ => make_keyword("statement"),
     keyword_execute: _ => make_keyword("execute"),
     keyword_procedure: _ => make_keyword("procedure"),
+    keyword_object_id: _ => make_keyword("object_id"),
 
     // Hive Keywords
     keyword_external: _ => make_keyword("external"),
@@ -2277,6 +2278,21 @@ module.exports = grammar({
       $.identifier,
     ),
 
+    object_id: $ => seq(
+      $.keyword_object_id,
+      wrapped_in_parenthesis(
+        seq(
+          alias($._literal_string, $.literal),
+          optional(
+            seq(
+              ',',
+              alias($._literal_string, $.literal),
+            ),
+          ),
+        ),
+      ),
+    ),
+
     object_reference: $ => choice(
       seq(
         field('database', $.identifier),
@@ -3427,6 +3443,7 @@ module.exports = grammar({
         $.interval,
         $.between_expression,
         $.parenthesized_expression,
+        $.object_id,
       )
     ),
 
