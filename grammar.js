5129061608da71146c813e13c32a54f4b13645c8
matthias-Q <matthias-Q@users.noreply.github.com>
2026-01-04 14:42:34 +0000

    deploy: 39489e8728db978f832b24873e995023fcd8e5c3

--- grammar.js
+++ grammar.js
@@ -1820,6 +1820,7 @@ module.exports = grammar({
         $.alter_database,
         $.alter_role,
         $.alter_sequence,
+        $.alter_policy,
       ),
     ),
 
@@ -2184,6 +2185,59 @@ module.exports = grammar({
           choice(
             choice($.keyword_logged, $.keyword_unlogged),
             seq($.keyword_schema, $.identifier)
+          ),
+        ),
+      ),
+    ),
+
+    // Postgres row level security
+    alter_policy: $ => prec.right(
+      seq(
+        $.keyword_alter,
+        $.keyword_policy,
+        $.object_reference,
+        $.keyword_on,
+        $.object_reference,
+        choice(
+          $.rename_object,
+          seq(
+            optional(
+              seq(
+                $.keyword_to,
+                choice(
+                  $.object_reference,
+                  $.keyword_public,
+                  $.keyword_current_role,
+                  $.keyword_current_user,
+                  $.keyword_session_user,
+                ),
+                repeat(
+                  seq(
+                    ',',
+                    choice(
+                      $.object_reference,
+                      $.keyword_public,
+                      $.keyword_current_role,
+                      $.keyword_current_user,
+                      $.keyword_session_user,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            optional(
+              seq(
+                $.keyword_using,
+                $.parenthesized_expression,
+              ),
+            ),
+            optional(
+              seq(
+                $.keyword_with,
+                $.keyword_check,
+                $.parenthesized_expression,
+              ),
+            ),
           ),
         ),
       ),
